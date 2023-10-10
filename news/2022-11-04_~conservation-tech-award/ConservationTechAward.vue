ba7c84c993830db3d19a4d41f028e1555fa8fa7f
Clemens Hahn <clemens@couchbits.com>
2023-10-10 15:55:49 +0200

    add old news items

---
+++ news/2022-11-04_~conservation-tech-award/ConservationTechAward.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" >
+        <h3>MoveApps wins Conservation Tech Award</h3>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+      <v-col cols="12" md="4" sm="3" class="align-self-start">
+        <v-img :src="image" max-height="250px" contain />
+      </v-col>
+      <v-col cols="12" md="8" sm="9">
+        <p>We are delighted to announce that Earth Ranger has named MoveApps as a grantee of the Conservation Technology Award for 2022. MoveApps was one of only two organizations to be awarded this no-strings-attached grant of $15,000.</p>
+        <p>We are thrilled to be recognized for our work leveraging tracking data to safeguard wildlife across the globe. We pass on our heartfelt thanks to the users and developers who have contributed to this success. <router-link :to="{ name: 'GetInvolved'}">This is an award we all share!</router-link></p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConservationTechAward",
+
+  data: () => ({
+    images: [
+        require('@/views/Marketing/News/image0.jpeg'),
+        require('@/views/Marketing/News/image1.jpeg')
+      ]
+  }),
+  computed: {
+    image() {
+      let min = 0
+      let max = this.images.length - 1
+      let idx = Math.floor(Math.random() * (max - min + 1) + min)
+      if (idx >= 0 && idx < this.images.length) { // paranoia :D
+        return this.images[idx]
+      }
+      return this.images[0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ news/2023-02-10_~ctmm-job-advert/CtmmCodeDevelopmentPosition.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12" >
+        <h3>Short-term code development position: Bring ctmm to MoveApps</h3>
+      </v-col>
+    </v-row>
+    <v-row align="center">
+      <v-col cols="12" md="4" sm="3" class="align-self-start">
+        <v-img :src="image" max-height="250px" contain />
+      </v-col>
+      <v-col cols="12" md="8" sm="9">
+        <p>We are looking for an experienced R-programmer that can <blank-target-link :url="pdf">develop a sequence of Apps for MoveApps</blank-target-link> using the <code>ctmm</code> (continuous-time movement modeling) R package. The assignment can be started anytime and must be finalized by 12 June 2023. The Apps have to be set up in collaboration with the MoveApps development team and under advice by the ctmm package developers.</p>
+        <p>Payment is $15,000 USD paid half at the midway point and half at the successful end of the project. Work can be performed remotely or in collaboration with one of the institutes of the ctmm team or one of the institutes of the MoveApps team. <a href="mailto:info@moveapps.org">Contact us!</a></p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import image0 from "@/views/Marketing/News/CtmmMoveApps/ctmm2moveapps.png";
+import BlankTargetLink from "@/components/Helper/BlankTargetLink.vue";
+
+export default {
+  name: "CtmmCodeDevelopmentPosition",
+  components: {BlankTargetLink},
+
+  data: () => ({
+    images: [
+        image0
+      ],
+    pdf: "/news/Ctmm_to_MoveApps_-_Job_Advert.pdf"
+  }),
+  computed: {
+    image() {
+      return this.images[0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
